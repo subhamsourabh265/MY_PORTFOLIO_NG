e60b527637b5f2a2b9d9556a2c6b58461d3887de
Subham Sourabh <sourabh.shubham120@gmail.com>
2026-09-20 10:18:44 +0530

    feat: update resume link to version 3 and remove outdated version 2

--- src/app/data/profile.ts
+++ src/app/data/profile.ts
@@ -1,5 +1,5 @@
 export const PROFILE = {
   name: 'SUBHAM SOURABH',
   email: 'sourabh.shubham120@gmail.com',
-  resume: 'assets/Subham_Sourabh_Lead_Angular_Resume_V2.pdf',
+  resume: 'assets/Subham_Sourabh_Lead_Angular_Resume_V3.pdf',
 } as const;
